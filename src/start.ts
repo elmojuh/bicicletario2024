246a24111ef5f68f2cfb0421b331b825485b97f8
elmojuh <junior.fool.skull@gmail.com>
2024-09-02 06:42:55 -0300

    05 ajuste integração

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,6 @@
 import app from './server';
 
-const port = 80; // ou 443 para HTTPS
+const port = 3000; // ou 443 para HTTPS
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
